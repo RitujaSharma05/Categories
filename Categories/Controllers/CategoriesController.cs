697031cd941d0a8ee8371f58ce7f9c8775c56579
Admin <Admin@DESKTOP-TRVHCHQ>
2025-06-16 11:44:06 +0530

    Add project files.

---
+++ Categories/Controllers/CategoriesController.cs
@@ -0,0 +1,35 @@
+﻿using Categories.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Categories.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = CategoriesRepository.GetCategories();
+            return View(categories);
+        }
+        public IActionResult Edit([FromRoute]int? id)
+        {
+
+            //var category = new Category{ Id = id.HasValue? id.Value: 0};
+            var category = CategoriesRepository.GetCategoryById(id.HasValue ? id.Value : 0);
+            return View(category);
+        }
+
+        [HttpPost]
+
+        public IActionResult Edit(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                CategoriesRepository.UpdateCategory(category.Id, category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+            
+        }
+
+    }
+}
---
+++ Categories/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Categories.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = String.Empty;
+        [Required]
+        public string Description { get; set; } = String.Empty;
+    }
+}
